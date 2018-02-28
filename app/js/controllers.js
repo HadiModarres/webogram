a0bd135246d1855bc6a19f8313d14e8a9544aeb2
Tony Bogdanov <tonybogdanov@gmail.com>
2018-02-28 15:37:26 +0200

    Webogram now shows the auth popup if the user is not logged in with Enya

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,18 +14,40 @@ angular.module('myApp.controllers', ['myApp.i18n'])
 
   .controller('AppWelcomeController', function ($scope, $location, MtpApiManager, ChangelogNotifyService, LayoutSwitchService) {
     MtpApiManager.getUserID().then(function (id) {
-      if (id) {
-        $location.url('/im')
-        return
-      }
-      if (location.protocol == 'http:' &&
-        !Config.Modes.http &&
-        Config.App.domains.indexOf(location.hostname) != -1) {
-        location.href = location.href.replace(/^http:/, 'https:')
-        return
-      }
-      $location.url('/login')
-    })
+      var logged_in = false,
+          urls = {};
+
+      $.ajax({
+          method: 'GET',
+          url: '/wp-admin/admin-ajax.php',
+          data: {
+            action: 'authenticator',
+            phase: 'api-status',
+            referrer: window.top.location.href === document.location.href ? '/' : window.top.location.href
+          },
+          dataType: 'json'
+      }).done(function (response) {
+        logged_in = response['logged_in'];
+        urls = response['urls'];
+      }).complete(function () {
+        if (!logged_in) {
+          window.top.location = urls.login;
+          return;
+        }
+
+        if (id) {
+            $location.url('/im');
+            return;
+        }
+
+        if (location.protocol == 'http:' && !Config.Modes.http && Config.App.domains.indexOf(location.hostname) != -1) {
+            location.href = location.href.replace(/^http:/, 'https:');
+            return;
+        }
+
+        $location.url('/login');
+      });
+    });
 
     ChangelogNotifyService.checkUpdate()
     LayoutSwitchService.start()
